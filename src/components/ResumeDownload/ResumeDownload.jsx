ef557efcbc1d75c72153981eebc37a8f47a0a5aa
Daniel Gerlach <danielg-gerlach@outlook.de>
2025-05-08 16:41:14 +0200

    adapted pictures linked

--- src/components/ResumeDownload/ResumeDownload.jsx
+++ src/components/ResumeDownload/ResumeDownload.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import { FaExternalLinkAlt } from 'react-icons/fa';
 import './ResumeDownload.css';
+import ResumePic from "../../assets/images2/resume/Resume_Daniel_Gerlach.png";
 
 function ResumeDownload() {
   // Google Docs-Link zum Lebenslauf
@@ -16,7 +17,7 @@ function ResumeDownload() {
           <div className="resume-content">
             <h3 className="resume-title">My resume for everyone who's interested!</h3>
             <p className="resume-description">
-            My resume is available online in Google Drive. It contains  information about
+              My resume is available online in Google Drive. It contains information about
               my education, professional experience, technical skills and projects.
               You can view it directly online or download it.
             </p>
@@ -34,11 +35,17 @@ function ResumeDownload() {
           
           <div className="resume-preview">
             <div className="resume-preview-inner">
-              <img 
-                src="/public/images2/resume/Resume_Daniel_Gerlach.png" 
-                alt="Vorschau des Lebenslaufs" 
-                className="resume-preview-img" 
-              />
+              {ResumePic ? (
+                <img 
+                  src={ResumePic} 
+                  alt="Resume Preview" 
+                  className="resume-preview-img" 
+                />
+              ) : (
+                <div className="resume-placeholder">
+                  Resume Preview Not Available
+                </div>
+              )}
             </div>
           </div>
         </div>
